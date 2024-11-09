06c5780cac785cad25cabf137f27fa3c36413517
lennoncastro <lennon.castro.92@gmail.com>
2024-11-08 22:21:34 -0300

    feat: added pull requests page improvements

--- lib/features/pull_requests/data/models/pull_request_model.dart
+++ lib/features/pull_requests/data/models/pull_request_model.dart
@@ -8,9 +8,7 @@ class PullRequest with _$PullRequest {
   const factory PullRequest({
     @Default('') String url,
     @Default(0) int id,
-    @Default(0) int number,
     @Default('') String state,
-    @Default(false) bool locked,
     @Default('') String title,
     @Default('') String body,
     @Default(User()) User user,
--- lib/features/pull_requests/presentation/blocs/pull_requests_bloc.dart
+++ lib/features/pull_requests/presentation/blocs/pull_requests_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:github_repos/features/pull_requests/domain/src.dart';
 import 'package:github_repos/features/pull_requests/presentation/blocs/pull_requests_event.dart';
@@ -10,27 +12,29 @@ final class PullRequestsBloc
   PullRequestsBloc({
     required this.getPullRequestsByRepoAndAuthor,
   }) : super(PullRequestsState()) {
-    on<GetByUserAndRepo>((
-      GetByUserAndRepo event,
-      Emitter<PullRequestsState> emit,
-    ) async {
-      try {
-        emit(state.copyWith(status: PullRequestsStatus.loading));
-        final pullRequests = await getPullRequestsByRepoAndAuthor(
-          user: event.user,
-          repo: event.repo,
-        );
-        emit(
-          state.copyWith(
-            status: PullRequestsStatus.success,
-            pullRequests: pullRequests,
-          ),
-        );
-      } catch (_) {
-        emit(state.copyWith(status: PullRequestsStatus.unknownError));
-      }
-    });
+    on<GetByUserAndRepo>(_onGetByUserAndRepo);
   }
 
   final GetPullRequestsByUserAndRepo getPullRequestsByRepoAndAuthor;
+
+  FutureOr<void> _onGetByUserAndRepo(
+    GetByUserAndRepo event,
+    Emitter<PullRequestsState> emit,
+  ) async {
+    try {
+      emit(state.copyWith(status: PullRequestsStatus.loading));
+      final pullRequests = await getPullRequestsByRepoAndAuthor(
+        user: event.user,
+        repo: event.repo,
+      );
+      emit(
+        state.copyWith(
+          status: PullRequestsStatus.success,
+          pullRequests: pullRequests,
+        ),
+      );
+    } catch (_) {
+      emit(state.copyWith(status: PullRequestsStatus.unknownError));
+    }
+  }
 }
