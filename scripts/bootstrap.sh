e6362af78520f5e894a28c1095191341eb5c4a8d
lennoncastro <lennon.castro.92@gmail.com>
2024-11-12 08:57:45 -0300

    feat: added bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,2 +1,4 @@
 # shellcheck disable=SC2044
+flutter clean
+flutter pub get
 dart run build_runner build --delete-conflicting-outputs
--- scripts/generate_code_coverage_report.sh
+++ scripts/generate_code_coverage_report.sh
@@ -1,2 +1,3 @@
+# shellcheck disable=SC2044
 flutter test --coverage
 genhtml coverage/lcov.info -o coverage/html
